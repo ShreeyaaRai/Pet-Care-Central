12d654a633fffb4aebaa2069381346ba5158b0b8
ShreeyaaRai <159517130+ShreeyaaRai@users.noreply.github.com>
2024-09-04 17:14:49 +0530

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,12 @@
+
+<?php
+
+
+
+// Grab User submitted information
+$email = $_POST["username"];
+$pass = $_POST["password"];
+
+if($email=='senor'&& $pass=='senorita20')
+echo "Connected successfully";
+?>
